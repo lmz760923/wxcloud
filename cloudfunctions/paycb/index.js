4af0f438b35dfda57bdf7b4ea96b2f0de7361edb
lmz760923 <15919731630@163.com>
2025-02-20 20:51:06 +0800

    20250220

--- cloudfunctions/paycb/index.js
+++ cloudfunctions/paycb/index.js
@@ -32,76 +32,7 @@ class AesUtil {
 
 // 云函数主入口
 exports.main = async (event, context) => {
-  // 测试报文
-  // var event = {
-  //   body: '{"id":"809eb6dd-9d48-5d1b-bbd3-ed87c5de35bc","create_time":"2024-12-26T21:48:52+08:00","resource_type":"encrypt-resource","event_type":"TRANSACTION.SUCCESS","summary":"支付成功","resource":{"original_type":"transaction","algorithm":"AEAD_AES_256_GCM","ciphertext":"u/Rja4UU3arbZqKW3eTharwbXD33ynNFva29UlVQu8NkxMZRwk0PNdPRCFaJ/EzyDwEDR7ZcY3mL44de9cXO+/LfjxZffcjlYkLhgy6cZCCGRhj4Hx1i/BLgI3OUpkG91zpkf5NQ6z4jEXHQhE4k9CzD5ln0NyubG2AQQf38SP9PS9mYfCaHdkwvO0a6yx6hqjuOOZkpmtfxAHsPwoJs60+TNhhmwnbUmpTlKgtk1zR7EkT/GTU8/AusmUf/NLE6SNlmV5zDMow0Nw56kVEjs6KZ6eZTlP2R4z93yDRSYJjxu0XTf36+Ys6qd4co7a2Rs5ExQZkyzPWJnksXQdkATixdNnFxijYaDHQYMxuF7T4jtk6q6yNyq04A4FGoBxJixg84t35bEWrVgQMRh7BaRtTsW6InaUE2o3tXxP/GPIs4jGCkKe628CcraBdIfsrnELvBYtlFb29sp0jfEK/0ugNbtK4d7qBhWtnVN4FXMUAmwve1btNLlrsgQc+GYp6XTLWSMzPQ+04tmO1HydLGAmHn33r40raeOsH/fKvFevfk8YMEQz4kQ2yp8tlrBFy645QGjHc+YVs6klUIVA==","associated_data":"transaction","nonce":"erSnoFzfnp08"}}',
-  //   headers: {
-  //     '5e645e31d98275b17df2fc9a2447c4d1': 'tag',
-  //     accept: '*/*',
-  //     'cache-control': 'no-cache',
-  //     'content-length': '923',
-  //     'content-type': 'application/json',
-  //     pragma: 'no-cache',
-  //     'user-agent': 'Mozilla/4.0',
-  //     'wechatpay-nonce': '33kNrV0Kl6ierzPzYNlL6N7MpBKdWAi8',
-  //     'wechatpay-serial': 'PUB_KEY_ID_0117004403622024121000218500000722',
-  //     'wechatpay-signature': 'ha3cq9ypfU39E4Cd3EWrJyeNUAH7TEp2bXYgBgbUNn/EW5XKyOG/dsx7sZi9aEIiVdeu4GEphFuHSp+Mu88mhDGZQFENbdAYp3ApOgszMvLPNhNb4lg5S0CQg6fMesb+lrYBVNzuT5NoCAjnr4crEA5IJVlYUCCADeK6+6LE7BmVfzKCQHS1a9WxjQy9H7skUBpqNUDhdneUmHSyD4xkMuRFMAn5Gvwao9wgiL1AHaRSUe25JZvere0M9cHdqUQxUazCnoI6OMge9rGBr02w5kq2z4VvQsb6biFYAJcT23wkLuix/THQWQK0rsWCEJCVG86rtVCwk1S5BIoeH4Ds5w==',
-  //     'wechatpay-signature-type': 'WECHATPAY2-SHA256-RSA2048',
-  //     'wechatpay-timestamp': '1735220932',
-  //     'x-cloudbase-context': 'H4sIAAAAAAAA/0TLvQrCMBRH8Xf5zy2k9AOb1cnF0VVuk4tcMElJmmgV310CguM58HuDfTlZaDiSlKXtbuvzFR6kpoPtaR7QIIuHRqeU6udxVNNQZ+JYxPCZHENjpf24/O+FY5JQlc3O7W35dYMUcjRVbGa5mruw3/D5BgAA///7+CQAhQAAAA==',
-  //     'x-cloudbase-request-id': 'bc21d59c-64b1-4da7-876b-509d81680d42',
-  //     'x-cloudbase-trace': 'MTNlZDk5MzI1ODdiNDI2YmI0MjkwYjMyNWY0MjJhMTQsYWZlNzMxNWI5NGVkNDM0MjlhNDQ1YTQ4YmU2YWNhYWIsb24=',
-  //     'x-forwarded-for': '101.226.103.14',
-  //     'x-forwarded-proto': 'https',
-  //     'x-nws-log-uuid': '10533617663847252198',
-  //     'x-origin-host': 'maisui-1gpxzowa068d3a94-1331667450.ap-shanghai.app.tcloudbase.com',
-  //     'x-real-ip': '101.226.103.14',
-  //     'x-request-id': 'bc21d59c-64b1-4da7-876b-509d81680d42',
-  //     'x-tencent-ua': 'Qcloud'
-  //   },
-  //   httpMethod: 'POST',
-  //   isBase64Encoded: false,
-  //   multiValueHeaders: {
-  //     '5e645e31d98275b17df2fc9a2447c4d1': ['tag'],
-  //     accept: ['*/*'],
-  //     'cache-control': ['no-cache'],
-  //     'content-length': ['923'],
-  //     'content-type': ['application/json'],
-  //     pragma: ['no-cache'],
-  //     'user-agent': ['Mozilla/4.0'],
-  //     'wechatpay-nonce': ['33kNrV0Kl6ierzPzYNlL6N7MpBKdWAi8'],
-  //     'wechatpay-serial': ['PUB_KEY_ID_0117004403622024121000218500000722'],
-  //     'wechatpay-signature': [
-  //       'ha3cq9ypfU39E4Cd3EWrJyeNUAH7TEp2bXYgBgbUNn/EW5XKyOG/dsx7sZi9aEIiVdeu4GEphFuHSp+Mu88mhDGZQFENbdAYp3ApOgszMvLPNhNb4lg5S0CQg6fMesb+lrYBVNzuT5NoCAjnr4crEA5IJVlYUCCADeK6+6LE7BmVfzKCQHS1a9WxjQy9H7skUBpqNUDhdneUmHSyD4xkMuRFMAn5Gvwao9wgiL1AHaRSUe25JZvere0M9cHdqUQxUazCnoI6OMge9rGBr02w5kq2z4VvQsb6biFYAJcT23wkLuix/THQWQK0rsWCEJCVG86rtVCwk1S5BIoeH4Ds5w=='
-  //     ],
-  //     'wechatpay-signature-type': ['WECHATPAY2-SHA256-RSA2048'],
-  //     'wechatpay-timestamp': ['1735220932'],
-  //     'x-cloudbase-context': [
-  //       'H4sIAAAAAAAA/0TLvQrCMBRH8Xf5zy2k9AOb1cnF0VVuk4tcMElJmmgV310CguM58HuDfTlZaDiSlKXtbuvzFR6kpoPtaR7QIIuHRqeU6udxVNNQZ+JYxPCZHENjpf24/O+FY5JQlc3O7W35dYMUcjRVbGa5mruw3/D5BgAA///7+CQAhQAAAA=='
-  //     ],
-  //     'x-cloudbase-request-id': ['bc21d59c-64b1-4da7-876b-509d81680d42'],
-  //     'x-cloudbase-trace': [
-  //       'MTNlZDk5MzI1ODdiNDI2YmI0MjkwYjMyNWY0MjJhMTQsYWZlNzMxNWI5NGVkNDM0MjlhNDQ1YTQ4YmU2YWNhYWIsb24='
-  //     ],
-  //     'x-forwarded-for': ['101.226.103.14'],
-  //     'x-forwarded-proto': ['https'],
-  //     'x-nws-log-uuid': ['10533617663847252198'],
-  //     'x-origin-host': [
-  //       'maisui-1gpxzowa068d3a94-1331667450.ap-shanghai.app.tcloudbase.com'
-  //     ],
-  //     'x-real-ip': ['101.226.103.14'],
-  //     'x-request-id': ['bc21d59c-64b1-4da7-876b-509d81680d42'],
-  //     'x-tencent-ua': ['Qcloud']
-  //   },
-  //   path: '/',
-  //   queryStringParameters: {},
-  //   requestContext: {
-  //     appId: '1331667450',
-  //     envId: 'maisui-1gpxzowa068d3a94',
-  //     requestId: 'bc21d59c-64b1-4da7-876b-509d81680d42',
-  //     uin: '100039550644'
-  //   }
-  // };
+  
   const key = Buffer.from(apiV3Key, 'utf-8');
   var body = event.body;
   try {
